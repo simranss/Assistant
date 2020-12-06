ac53b1cb1d036811d1ddaf0e740d1e77c3ecfd15
Simran <simran.s.sharma00@gmail.com>
2020-12-06 22:32:11 +0530

    Correcting the imports

--- demo.py
+++ demo.py
@@ -1,12 +1,14 @@
-import speech_recognition as sr
-import pyttsx3
-import wikipedia
-from ecapture import ecapture as ec
-import datetime
-import os
-import time
-import webbrowser
-import subprocess
-import json
-import requests
+import speech_recognition as sr #speech to text
+from gtts import gTTS #text to speech
+import wikipedia #for wikipedia data
+from ecapture import ecapture as ec #clicking pictures from the camera
+import datetime #date and time
+import os #system and platform oriented commands
+from time import ctime #for providing time
+import time #for taking pauses
+import webbrowser #extracting data from the web
+import subprocess #commands like logoff shutdown
+import json #storing and retrieving data
+import requests #handles all the http requessts
+
 
