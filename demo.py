cb5335dcb694f165a3c86abcded72ac68da016d2
Simran <simran.s.sharma00@gmail.com>
2020-12-06 10:44:13 +0530

    Importing the libraries

---
+++ demo.py
@@ -0,0 +1,12 @@
+import speech_recognition as sr
+import pyttsx3
+import wikipedia
+from ecapture import ecapture as ec
+import datetime
+import os
+import time
+import webbrowser
+import subprocess
+import json
+import requests
+
